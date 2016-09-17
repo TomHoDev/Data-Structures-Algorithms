177c696847a586b15a74c3986c79f3b07800cc9c
tomhodev <datomster21@gmail.com>
2016-09-17 17:30:21 -0500

    add get method and test

--- FixedArray/fixed_array.rb
+++ FixedArray/fixed_array.rb
@@ -4,4 +4,8 @@ attr_accessor :array
   def initialize(size)
     @array = Array.new(size)
   end
+
+  def get(index) 
+    @array[index]
+  end
 end
--- FixedArray/fixed_array_spec.rb
+++ FixedArray/fixed_array_spec.rb
@@ -1,8 +1,19 @@
 require_relative 'fixed_array'
 
 describe FixedArray do
+  let(:fa) {FixedArray.new(3)}
+
   it 'instantiate a new array with given size' do 
     expect(FixedArray.new(3).array).to eq ([nil,nil,nil])
+  end
+
+  it 'gets the value at an index' do 
+    fa.array = [1,2,3]
+    expect(fa.get(1)).to eq(2)
+  end
+
+  it 'returns an error when an invalid index is given' do 
+    
   end
     
 end
