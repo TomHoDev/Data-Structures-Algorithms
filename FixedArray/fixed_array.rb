2ce70b1d4cad03cc964d7691e115707d00048998
tomhodev <datomster21@gmail.com>
2016-09-17 18:06:02 -0500

    add feature to set indexes with tests

--- FixedArray/fixed_array.rb
+++ FixedArray/fixed_array.rb
@@ -6,11 +6,26 @@ attr_accessor :array
   end
 
   def get(index)
-    if index >= @array.length || index < 0
+    if out_of_bounds?(index)
       raise IndexOutOfBoundsError
     end 
     @array[index]
   end
+
+  def set(index, value)
+    if out_of_bounds?(index)
+      raise IndexOutOfBoundsError
+    end 
+    @array[index] = value
+  end
+
+  private 
+    def out_of_bounds?(index)
+      if index >= @array.length || index < 0
+        return true
+      end
+      false 
+    end
 end
 
 class IndexOutOfBoundsError < StandardError
--- FixedArray/specs/fixed_array_spec.rb
+++ FixedArray/specs/fixed_array_spec.rb
@@ -3,19 +3,38 @@ require_relative '../fixed_array'
 describe FixedArray do
   let(:fa) {FixedArray.new(3)}
 
-  it 'instantiate a new array with given size' do 
-    expect(FixedArray.new(3).array).to eq ([nil,nil,nil])
+  describe 'FixedArray#new' do
+    it 'instantiate a new array with given size' do 
+      expect(FixedArray.new(3).array).to eq ([nil,nil,nil])
+    end
   end
 
-  it 'gets the value at an index' do 
-    fa.array = [1,2,3]
-    expect(fa.get(1)).to eq(2)
+  describe 'FixedArray#get' do
+    it 'gets the value at an index' do 
+      fa.array = [1,2,3]
+      expect(fa.get(1)).to eq(2)
+    end
+
+    it 'returns an error when an invalid index is given' do 
+      expect { fa.get(4) }.to raise_error(IndexOutOfBoundsError)
+      expect { fa.get(-1) }.to raise_error(IndexOutOfBoundsError)
+    end
   end
 
-  it 'returns an error when an invalid index is given' do 
-    expect { fa.get(4) }.to raise_error(IndexOutOfBoundsError)
-    expect { fa.get(-1) }.to raise_error(IndexOutOfBoundsError)
-  end
-    
+  describe 'FixedArray#get' do
+    it 'changes the fixed array' do 
+      expect { fa.set(0,2)}.to change{fa.get(0)}.from(nil).to(2)
+    end
+
+    it 'returns the new element' do 
+      expect(fa.set(0,2)).to eq(2)
+    end
+
+    it 'returns an error when an invalid index is given' do 
+      expect { fa.set(4,0) }.to raise_error(IndexOutOfBoundsError)
+      expect { fa.set(-1,0) }.to raise_error(IndexOutOfBoundsError)
+    end
+
+  end  
 end
 
