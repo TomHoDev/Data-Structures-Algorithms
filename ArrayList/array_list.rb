30e13daa66e946030741ffbbd1bca44e25ee836d
tomhodev <datomster21@gmail.com>
2016-09-19 18:20:29 -0500

    add tests and method to add elements to the array list

--- ArrayList/array_list.rb
+++ ArrayList/array_list.rb
@@ -4,7 +4,19 @@ class ArrayList
   attr_accessor :array 
 
   def initialize(size)
-    @array = FixedArray.new(size).array
+    @array = FixedArray.new(size)
+  end
+
+  def add(element)
+    @array.array.push(element).last
+  end
+
+  def get(index)
+    @array.get(index)
+  end
+
+  def set(index, value)
+    @array.set(index,value)
   end
 
 end
